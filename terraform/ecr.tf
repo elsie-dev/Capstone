145239dfefbd299bdec9b7f5dc879cd4ead5a9d4
Elsie Marion Omondi <emomondi@safaricom.co.ke>
2024-02-16 11:36:40 +0300

    updated ecr file

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "repository" {
-  name                 = "kubernetes"
+  name                 = "altschoolcapstone"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
