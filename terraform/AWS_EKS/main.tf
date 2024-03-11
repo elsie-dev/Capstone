beea2629ac1248a3ef30b64e7e99abb5341ccc3a
elsie-dev <elcymao@gmail.com>
2024-03-11 23:36:36 +0300

    deleted deployment file

--- terraform/AWS_EKS/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "us-east-1"
-    # version = ">= 3.40.0"    
-    
-}
-data "aws_availability_zones" "azs" {
-    state = "available"
-}
--- terraform/cluster.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_eks_cluster" "capstone_cluster" {
-name = "capstone-cluster"
-role_arn = aws_iam_role.capstone_cluster_role.arn
-
-vpc_config {
-subnet_ids = ["subnet-10.0.101.0/24", "subnet-10.0.102.0/24"]
-}
-}
-
